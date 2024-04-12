8ee7f98b6dfc46727ff2be4dfff6b0c800bf63bf
rems <tech1.rems@gmail.com>
2024-04-12 19:12:37 +0000

    Created a program to add two number. It consists of a function that take two numbers as arguments, adds them and print the result.

---
+++ add-two-numbers.py
@@ -0,0 +1,10 @@
+# program that will add two numbers
+
+def add_two_numbers(num1, num2):
+    result = num1 + num2
+    print(result)
+
+num1 = int(input('Enter first number: '))
+num2 = int(input('Enter second number: '))
+
+add_two_numbers(num1, num2)
